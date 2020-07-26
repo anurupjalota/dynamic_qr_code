22fb1521e663ad0dfb903bb7ad17fb6adc409487
anurupjalota <anurupjalota@gmail.com>
2020-07-26 12:45:59 +0530

    try1

---
+++ try.py
@@ -0,0 +1,13 @@
+import pyqrcode 
+import png 
+from pyqrcode import QRCode 
+  
+  
+# String which represents the QR code 
+s = "www.geeksforgeeks.org"
+  
+# Generate QR code 
+url = pyqrcode.create(s) 
+  
+# Create and save the svg file naming "myqr.svg" 
+url.svg("myqr.svg", scale = 8) 
